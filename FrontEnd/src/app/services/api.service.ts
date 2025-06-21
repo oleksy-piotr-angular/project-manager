e69108c5827d6f4f22d1d5fbad9eb0542a562511
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-06-21 21:33:31 +0200

    add enironments with Update for ApiService

--- FrontEnd/src/app/services/api.service.ts
+++ FrontEnd/src/app/services/api.service.ts
@@ -2,13 +2,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { environment } from '../../environments/environment'; //TODO
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000'; // TODO: Get from environment variables
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
