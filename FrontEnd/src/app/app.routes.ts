eac30e1c9cf8191016a785642c7027e74fd4335f
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-06-21 19:11:11 +0200

    implementing planned routes in the application with guards

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { noAuthGuard } from './guards/no-auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: 'dashboard',
+    //TODO loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'projects/:id',
+    //TODO loadComponent: () => import('./pages/project-details/project-details.component').then(m => m.ProjectDetailsComponent),
+    canActivate: [authGuard],
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' }, // lub strona 404
+];
