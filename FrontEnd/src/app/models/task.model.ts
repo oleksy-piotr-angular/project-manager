79fb6f863de2a79ac3b09d44a67375a1ced98f54
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-06-25 17:23:51 +0200

    refactor(task): Update Task model and TaskStatus based on actual definition

--- FrontEnd/src/app/models/task.model.ts
+++ FrontEnd/src/app/models/task.model.ts
@@ -1,8 +1,10 @@
+export type TaskStatus = 'todo' | 'in_progress' | 'done';
+
 export interface Task {
   id: string;
   projectId: string;
   title: string;
   description: string;
-  status: 'todo' | 'in_progress' | 'done';
+  status: TaskStatus;
   dueDate?: string; // ISO date string
 }
